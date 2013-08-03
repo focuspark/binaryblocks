3aef4bc7966cd25141c046b0e53cbb9fccead701
Jeremy Wyman <none@none>
2013-08-03 01:33:34 -0700

    Viewer project added to solution
    --HG--
    branch : dev1.x

---
+++ Viewer/MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace BinaryBlocks.Viewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(383, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 357);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(383, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(383, 333);
+            this.treeView1.TabIndex = 2;
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.FileOpenToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 379);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Binary Blocks Data Viewer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Viewer/MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryBlocks.Viewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private TreeNode ReadNode(Stream stream)
+        {
+            if (stream == null)
+                throw new System.ArgumentNullException();
+            if (!stream.CanRead || !stream.CanSeek)
+                throw new System.InvalidOperationException();
+
+            TreeNode node = new TreeNode();
+
+            BinaryBlocks.BinaryBlockReader blockReader = new BinaryBlocks.BinaryBlockReader(stream);
+            BinaryReader binaryReader = new BinaryReader(stream);
+
+            while (blockReader.Position < blockReader.Length)
+            {
+                BinaryBlocks.BinaryBlock block = blockReader.ReadBinaryBlock();
+
+                switch (block.Type ^ BlockType.List)
+                {
+                    case BlockType.Blob:
+                        {
+                            int size = binaryReader.ReadInt32();
+                            node.Text = String.Format("[{0}]<blob> = [{1}]", block.Ordinal, size);
+                            binaryReader.BaseStream.Position += size;
+                        }break;
+                    case BlockType.Byte:
+                        {
+                            node.Text = String.Format("[{0}]<byte> = {1}", block.Ordinal, blockReader.ReadByte());
+                        }break;
+                    case BlockType.Char:
+                        {
+                            node.Text = String.Format("[{0}]<char> = {1}", block.Ordinal, blockReader.ReadChar());
+                        } break;
+                    case BlockType.Double:
+                        {
+                            node.Text = String.Format("[{0}]<double> = {1}", block.Ordinal, blockReader.ReadDouble());
+                        }break;
+                    case BlockType.Enum:
+                        {
+                            node.Text = String.Format("[{0}]<enum> = {1}", block.Ordinal, blockReader.ReadUint());
+                        }break;
+                    case BlockType.Guid:
+                        {
+                            node.Text = String.Format("[{0}]<guid> = {1:N}", block.Ordinal, blockReader.ReadGuid());
+                        }break;
+                    case BlockType.Single:
+                        {
+                            node.Text = String.Format("[{0}]<single> = {1}", block.Ordinal, blockReader.ReadSingle());
+                        }break;
+                    case BlockType.Sint:
+                        {
+                            node.Text = String.Format("[{0}]<sint> = {1}", block.Ordinal, blockReader.ReadSint());
+                        }break;
+                    case BlockType.Slong:
+                        {
+                            node.Text = String.Format("[{0}]<slong> = {1}", block.Ordinal, blockReader.ReadSlong());
+                        }break;
+                    case BlockType.String:
+                        {
+                            node.Text = String.Format("[{0}]<string> = \"{1}\"", block.Ordinal, blockReader.ReadString());
+                        }break;
+                    case BlockType.Struct:
+                        {
+                            node.Text = String.Format("[{0}]<struct>", block.Ordinal);
+                            node.Nodes.Add(ReadNode(stream));
+                        } break;
+                    case BlockType.Timespan:
+                        {
+                            node.Text = String.Format("[{0}]<timespan> = {1}", block.Ordinal, blockReader.ReadTimespan());
+                        }break;
+                    case BlockType.Timestamp:
+                        {
+                            node.Text = String.Format("[{0}]<timestamp> = {1}", block.Ordinal, blockReader.ReadTimestamp());
+                        }break;
+                    case BlockType.Uint:
+                        {
+                            node.Text = String.Format("[{0}]<uint> = {1}", block.Ordinal, blockReader.ReadUint());
+                        }break;
+                    case BlockType.Ulong:
+                        {
+                            node.Text = String.Format("[{0}]<ulong> = {1}", block.Ordinal, blockReader.ReadUlong());
+                        }break;
+                    case BlockType.Unknown:
+                    default:
+                        throw new Exception();
+                }
+            }
+
+            return node;
+        }
+
+        private void FileOpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
