48346c49f6033becf4ba631dc279744f50d147dc
Jeremy Wyman <none@none>
2013-08-04 09:24:19 -0700

    Added a puplically available TreeListView control for better dispay of data.
    --HG--
    branch : dev1.x

--- Viewer/MainForm.Designer.cs
+++ Viewer/MainForm.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            CommonTools.TreeListColumn treeListColumn1 = new CommonTools.TreeListColumn("fieldname0", "Field");
+            CommonTools.TreeListColumn treeListColumn2 = new CommonTools.TreeListColumn("fieldname1", "Type");
+            CommonTools.TreeListColumn treeListColumn3 = new CommonTools.TreeListColumn("fieldname2", "Value");
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.treeView1 = new System.Windows.Forms.TreeView();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.dataTreeview = new CommonTools.TreeListView();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTreeview)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -46,22 +50,6 @@
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 357);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(383, 22);
-            this.statusStrip1.TabIndex = 1;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // treeView1
-            // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView1.Location = new System.Drawing.Point(0, 24);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(383, 333);
-            this.treeView1.TabIndex = 2;
-            // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -73,23 +61,53 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.FileOpenToolStripMenuItem_Click);
             // 
-            // Form1
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 357);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(383, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // dataTreeview
+            // 
+            treeListColumn1.AutoSizeMinSize = 0;
+            treeListColumn1.Width = 50;
+            treeListColumn2.AutoSizeMinSize = 0;
+            treeListColumn2.Width = 50;
+            treeListColumn3.AutoSizeMinSize = 0;
+            treeListColumn3.Width = 50;
+            this.dataTreeview.Columns.AddRange(new CommonTools.TreeListColumn[] {
+            treeListColumn1,
+            treeListColumn2,
+            treeListColumn3});
+            this.dataTreeview.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.dataTreeview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataTreeview.Images = null;
+            this.dataTreeview.Location = new System.Drawing.Point(0, 24);
+            this.dataTreeview.Name = "dataTreeview";
+            this.dataTreeview.Size = new System.Drawing.Size(383, 333);
+            this.dataTreeview.TabIndex = 2;
+            this.dataTreeview.Text = "treeListView1";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(383, 379);
-            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.dataTreeview);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Binary Blocks Data Viewer";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTreeview)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,7 +119,7 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.TreeView treeView1;
+        private CommonTools.TreeListView dataTreeview;
     }
 }
 
--- Viewer/MainForm.cs
+++ Viewer/MainForm.cs
@@ -16,16 +16,19 @@ namespace BinaryBlocks.Viewer
         public MainForm()
         {
             InitializeComponent();
+
+            using (FileStream stream = File.Open("complex.b3", FileMode.Open))
+            {
+                FillNodeCollection(dataTreeview.Nodes, stream);
+            }
         }
 
-        private TreeNode ReadNode(Stream stream)
+        private void FillNodeCollection(CommonTools.NodeCollection collection, Stream stream)
         {
             if (stream == null)
                 throw new System.ArgumentNullException();
             if (!stream.CanRead || !stream.CanSeek)
                 throw new System.InvalidOperationException();
-
-            TreeNode node = new TreeNode();
 
             BinaryBlocks.BinaryBlockReader blockReader = new BinaryBlocks.BinaryBlockReader(stream);
             BinaryReader binaryReader = new BinaryReader(stream);
@@ -33,79 +36,118 @@ namespace BinaryBlocks.Viewer
             while (blockReader.Position < blockReader.Length)
             {
                 BinaryBlocks.BinaryBlock block = blockReader.ReadBinaryBlock();
+                CommonTools.Node node = new CommonTools.Node();
+                collection.Add(node);
 
-                switch (block.Type ^ BlockType.List)
+                switch (block.Type)
                 {
                     case BlockType.Blob:
                         {
-                            int size = binaryReader.ReadInt32();
-                            node.Text = String.Format("[{0}]<blob> = [{1}]", block.Ordinal, size);
-                            binaryReader.BaseStream.Position += size;
-                        }break;
+                            int length = binaryReader.ReadInt32();
+                            node[0] = block.Ordinal;
+                            node[1] = "<blob>";
+                            node[2] = "length = " + length;
+                            binaryReader.BaseStream.Position += length;
+                        } break;
+                    case BlockType.BlobList:
+                        {
+                            int count = blockReader.ReadSint();
+                            node[0] = String.Format("#{0}#<blob[]> = {1}", block.Ordinal, count);
+                            for (int i = 0; i < count; i++)
+                            {
+                                CommonTools.Node child = new CommonTools.Node();
+                                int length = binaryReader.ReadInt32();
+                                child[0] = String.Format("#{0}#<blob> = [{1}]", block.Ordinal, length);
+                                binaryReader.BaseStream.Position += length;
+                                node.Nodes.Add(child);
+                            }
+                        } break;
                     case BlockType.Byte:
                         {
-                            node.Text = String.Format("[{0}]<byte> = {1}", block.Ordinal, blockReader.ReadByte());
-                        }break;
+                            node[0] = String.Format("#{0}#<byte> = {1}", block.Ordinal, blockReader.ReadByte());
+                        } break;
                     case BlockType.Char:
                         {
-                            node.Text = String.Format("[{0}]<char> = {1}", block.Ordinal, blockReader.ReadChar());
+                            node[0] = String.Format("#{0}#<char> = {1}", block.Ordinal, blockReader.ReadChar());
                         } break;
                     case BlockType.Double:
                         {
-                            node.Text = String.Format("[{0}]<double> = {1}", block.Ordinal, blockReader.ReadDouble());
-                        }break;
+                            node[0] = String.Format("#{0}#<double> = {1}", block.Ordinal, blockReader.ReadDouble());
+                        } break;
                     case BlockType.Enum:
                         {
-                            node.Text = String.Format("[{0}]<enum> = {1}", block.Ordinal, blockReader.ReadUint());
-                        }break;
+                            node[0] = String.Format("#{0}#<enum> = {1}", block.Ordinal, blockReader.ReadUint());
+                        } break;
                     case BlockType.Guid:
                         {
-                            node.Text = String.Format("[{0}]<guid> = {1:N}", block.Ordinal, blockReader.ReadGuid());
-                        }break;
+                            node[0] = String.Format("#{0}#<guid> = {1:N}", block.Ordinal, blockReader.ReadGuid());
+                        } break;
                     case BlockType.Single:
                         {
-                            node.Text = String.Format("[{0}]<single> = {1}", block.Ordinal, blockReader.ReadSingle());
-                        }break;
+                            node[0] = String.Format("#{0}#<single> = {1}", block.Ordinal, blockReader.ReadSingle());
+                        } break;
+                    case BlockType.SingleList:
+                        {
+                            int count = blockReader.ReadSint();
+                            node[0] = String.Format("#{0}#<single[]> = {1}", block.Ordinal, count);
+                            for (int i = 0; i < count; i++)
+                            {
+                                CommonTools.Node child = new CommonTools.Node();
+                                child[0] = String.Format("#{0}#<single> = {1}", block.Ordinal, blockReader.ReadSingle());
+                                node.Nodes.Add(child);
+                            }
+                        } break;
                     case BlockType.Sint:
                         {
-                            node.Text = String.Format("[{0}]<sint> = {1}", block.Ordinal, blockReader.ReadSint());
-                        }break;
+                            node[0] = String.Format("#{0}#<sint> = {1}", block.Ordinal, blockReader.ReadSint());
+                        } break;
                     case BlockType.Slong:
                         {
-                            node.Text = String.Format("[{0}]<slong> = {1}", block.Ordinal, blockReader.ReadSlong());
-                        }break;
+                            node[0] = String.Format("#{0}#<slong> = {1}", block.Ordinal, blockReader.ReadSlong());
+                        } break;
                     case BlockType.String:
                         {
-                            node.Text = String.Format("[{0}]<string> = \"{1}\"", block.Ordinal, blockReader.ReadString());
-                        }break;
+                            node[0] = block.Ordinal;
+                            node[1] = "<string>";
+                            node[2] = blockReader.ReadString();
+                        } break;
                     case BlockType.Struct:
                         {
-                            node.Text = String.Format("[{0}]<struct>", block.Ordinal);
-                            node.Nodes.Add(ReadNode(stream));
+                            node[0] = String.Format("#{0}#<struct>", block.Ordinal);
+                            int length = blockReader.ReadSint();
+                            FillNodeCollection(node.Nodes, new StructStream(stream, length));
+                        } break;
+                    case BlockType.StructList:
+                        {
+                            int count = blockReader.ReadSint();
+                            node[0] = String.Format("#{0}#<struct[]> = {1}", block.Ordinal, count);
+                            for (int i = 0; i < count; i++)
+                            {
+                                int length = blockReader.ReadSint();
+                                FillNodeCollection(node.Nodes, new StructStream(stream, length));
+                            }
                         } break;
                     case BlockType.Timespan:
                         {
-                            node.Text = String.Format("[{0}]<timespan> = {1}", block.Ordinal, blockReader.ReadTimespan());
-                        }break;
+                            node[0] = String.Format("#{0}#<timespan> = {1}", block.Ordinal, blockReader.ReadTimespan());
+                        } break;
                     case BlockType.Timestamp:
                         {
-                            node.Text = String.Format("[{0}]<timestamp> = {1}", block.Ordinal, blockReader.ReadTimestamp());
-                        }break;
+                            node[0] = String.Format("#{0}#<timestamp> = {1}", block.Ordinal, blockReader.ReadTimestamp());
+                        } break;
                     case BlockType.Uint:
                         {
-                            node.Text = String.Format("[{0}]<uint> = {1}", block.Ordinal, blockReader.ReadUint());
-                        }break;
+                            node[0] = String.Format("#{0}#<uint> = {1}", block.Ordinal, blockReader.ReadUint());
+                        } break;
                     case BlockType.Ulong:
                         {
-                            node.Text = String.Format("[{0}]<ulong> = {1}", block.Ordinal, blockReader.ReadUlong());
-                        }break;
+                            node[0] = String.Format("#{0}#<ulong> = {1}", block.Ordinal, blockReader.ReadUlong());
+                        } break;
                     case BlockType.Unknown:
                     default:
                         throw new Exception();
                 }
             }
-
-            return node;
         }
 
         private void FileOpenToolStripMenuItem_Click(object sender, EventArgs e)
