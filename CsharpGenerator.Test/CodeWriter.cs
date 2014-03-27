4ee1b4fad2d8103b464ac696a8da77438ad121ef
J Wyman <jwyman@microsoft.com>
2014-03-26 21:14:41 -0700

    added Test project

---
+++ CsharpGenerator.Test/CodeWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryBlocks.Test.CsharpGenerator
+{
+    [TestClass]
+    public class CodeWriter
+    {
+        [TestMethod]
+        public void BeginBlock()
+        {
+        }
+
+        [TestMethod]
+        public void EndBlock()
+        {
+        }
+
+        [TestMethod]
+        public void IndentLess()
+        {
+        }
+
+        [TestMethod]
+        public void IndentMore()
+        {
+        }
+
+        [TestMethod]
+        public void Merge()
+        {
+        }
+
+        [TestMethod]
+        public void Write()
+        {
+        }
+
+        [TestMethod]
+        public void WriteIndented()
+        {
+        }
+    }
+}
---
+++ CsharpGenerator.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CsharpGenerator.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CsharpGenerator.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f8972fd-e0d0-4783-a343-00f03f48ad04")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
