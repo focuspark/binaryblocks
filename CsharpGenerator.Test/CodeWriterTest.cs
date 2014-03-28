8d970fdaec6f7e319b6ad5a400d8cd1073635f00
Jeremy Wyman <focuspark@outlook.com>
2014-03-27 21:56:06 -0700

    adding missing files

---
+++ CsharpGenerator.Test/CodeWriterTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BinaryBlocks.CsharpGenerator;
+
+namespace BinaryBlocks.Test.CsharpGenerator
+{
+    [TestClass]
+    public class CodeWriterTest
+    {
+        [TestMethod]
+        public void BeginBlock()
+        {
+            CodeWriter writer = new CodeWriter();
+            writer.BeginBlock();
+            writer.BeginBlock();
+            writer.BeginBlock();
+            string result = writer.ToString();
+
+            Assert.AreEqual(result, "{\r\n    {\r\n        {\r\n");
+        }
+
+        [TestMethod]
+        public void EndBlock()
+        {
+            CodeWriter writer = new CodeWriter();
+            writer.BeginBlock();
+            writer.BeginBlock();
+            writer.BeginBlock();
+            writer.EndBlock();
+            writer.EndBlock();
+            writer.EndBlock();
+            string result = writer.ToString();
+
+            Assert.AreEqual(result, "{\r\n    {\r\n        {\r\n        }\r\n    }\r\n}\r\n");
+        }
+
+        [TestMethod]
+        public void IndentLess()
+        {
+            CodeWriter writer = new CodeWriter(5);
+            writer.Write("foo");
+            writer.IndentLess();
+            writer.Write("bar");
+            string result = writer.ToString();
+
+            Assert.AreEqual(result, "                    foo\r\n                bar\r\n");
+        }
+
+        [TestMethod]
+        public void IndentMore()
+        {
+            CodeWriter writer = new CodeWriter(4);
+            writer.Write("foo");
+            writer.IndentMore();
+            writer.Write("bar");
+            string result = writer.ToString();
+
+            Assert.AreEqual(result, "                foo\r\n                    bar\r\n");
+        }
+
+        [TestMethod]
+        public void Merge()
+        {
+            CodeWriter writer1 = new CodeWriter();
+            writer1.Write("class Foo");
+            writer1.BeginBlock();
+            writer1.Write("// body");
+            writer1.EndBlock();
+
+            CodeWriter writer2 = new CodeWriter();
+            writer2.Write("class Bar");
+            writer2.BeginBlock();
+            writer2.Write("// body");
+            writer2.EndBlock();
+
+            writer1.Merge(writer2);
+            string result = writer1.ToString();
+
+            Assert.AreEqual(result, "class Foo\r\n{\r\n    // body\r\n}\r\nclass Bar\r\n{\r\n    // body\r\n}\r\n");
+        }
+
+        [TestMethod]
+        public void Write()
+        {
+            CodeWriter writer = new CodeWriter();
+            writer.Write("foo bar bat baz");
+            string result = writer.ToString();
+
+            Assert.AreEqual(result, "foo bar bat baz\r\n");
+
+            writer = new CodeWriter();
+            writer.Write("{0} {1} {2} {3}", "foo", "bar", "bat", "baz");
+            result = writer.ToString();
+
+            Assert.AreEqual(result, "foo bar bat baz\r\n");
+        }
+
+        [TestMethod]
+        public void WriteIndented()
+        {
+            CodeWriter writer = new CodeWriter(1);
+            writer.Write("foo");
+            writer.WriteIndented("bar");
+            writer.Write("bat");
+            string result = writer.ToString();
+
+            Assert.AreEqual(result, "    foo\r\n        bar\r\n    bat\r\n");
+
+            writer = new CodeWriter(1);
+            writer.Write("foo");
+            writer.WriteIndented("bar{0}", "_bat");
+            writer.Write("baz");
+            result = writer.ToString();
+
+            Assert.AreEqual(result, "    foo\r\n        bar_bat\r\n    baz\r\n");
+        }
+    }
+}
---
+++ CsharpGenerator.Test/TextParserTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BinaryBlocks.CsharpGenerator;
+
+namespace BinaryBlocks.Test.CsharpGenerator
+{
+    [TestClass]
+    public class TextParserTest
+    {
+        [TestMethod]
+        public void SkipComment()
+        {
+            int index1 = 0;
+            TextParser.SkipComment("/* comment */ not_a_comment", ref index1);
+            Assert.IsTrue(index1 == 13);
+
+            int index2 = 0;
+            TextParser.SkipComment("// comment\r\nnot_a_comment", ref index2);
+            Assert.IsTrue(index2 == 12);
+
+            int index3 = 0;
+            TextParser.SkipComment(" // comment\r\nnot_a_comment", ref index3);
+            Assert.IsTrue(index3 == 13);
+
+            try
+            {
+                int index4 = 0;
+                TextParser.SkipComment("there's no comment here", ref index4);
+                Assert.Fail();
+            }
+            catch { /* this exception is a good thing */ }
+        }
+
+        [TestMethod]
+        public void ParseString()
+        {
+            int index1 = 0;
+            string result1 = TextParser.ParseString("\"a string\"", ref index1);
+            Assert.AreEqual(result1, "a string");
+            Assert.IsTrue(index1 == 9);
+
+            int index2 = 1;
+            string result2 = TextParser.ParseString(" \"a string\" ", ref index2);
+            Assert.AreEqual(result1, "a string");
+            Assert.IsTrue(index2 == 10);
+
+            int index3 = 13;
+            string result3 = TextParser.ParseString("not a string \"a string\"", ref index3);
+            Assert.AreEqual(result1, "a string");
+            Assert.IsTrue(index3 == 22);
+
+            try
+            {
+                int index4 = 0;
+                string result4 = TextParser.ParseString("not a string \"a string\"", ref index4);
+                Assert.Fail();
+            }
+            catch { /* this exception is a good thing */ }
+        }
+
+        [TestMethod]
+        public void PeekAny()
+        {
+            // this test should pass because only whitespace preceeds the first match
+            int index1 = 0;
+            if (!TextParser.PeekAny("  \t\r\n\t  [foo]", index1, '{', '['))
+            {
+                Assert.Fail();
+            }
+
+            // this test should fail because non-whitespace preceeds the first match
+            int index2 = 0;
+            if (TextParser.PeekAny("bar [foo]", index2, '{', '['))
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void SeekAny()
+        {
+            // this test should pass because only whitespace preceeds the first match
+            int index1 = 0;
+            TextParser.SeekAny("  \t\r\n\t  [foo]", ref index1, '{', '[');
+            Assert.IsTrue(index1 == 8);
+
+            // this test should pass because the index skips all non-whitespace preceeding the first match
+            int index2 = 3;
+            TextParser.SeekAny("bar [foo]", ref index2, '{', '[');
+            Assert.IsTrue(index2 == 4);
+
+            // this test should pass because the index skips all non-whitespace preceeding the first match
+            int index3 = 3;
+            TextParser.SeekAny("bar /* comment */ [foo]", ref index3, '{', '[');
+            Assert.IsTrue(index3 == 18);
+
+            // this test should fail because non-whitespace preceeds the first match
+            try
+            {
+                int index4 = 0;
+                TextParser.SeekAny("bar [foo]", ref index4, '{', '[');
+            }
+            catch { /* this exception is a good thing */ }
+        }
+
+        [TestMethod]
+        public void SeekNext()
+        {
+            int index1 = 0;
+            TextParser.SeekNext("\t  \r\n  next", ref index1);
+            Assert.IsTrue(index1 == 7);
+        }
+
+        [TestMethod]
+        public void ParseBlock()
+        {
+        }
+
+        [TestMethod]
+        public void ParseWord()
+        {
+        }
+    }
+}
